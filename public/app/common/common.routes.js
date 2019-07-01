459de92ab2e55434b902d58226c544d7449cfa14
Matyila Goodwish <goodwish@axxess.co.za>
2019-07-01 15:46:16 +0200

    update to configuration
    route config and common

---
+++ public/app/common/common.routes.js
@@ -0,0 +1,43 @@
+(function() {
+  const oren = angular.module("oren");
+  /**
+   * Configuration
+   * Goodwish Matyila
+   * Application Router
+   */
+  oren.config([
+    "$routeProvider",
+    "$locationProvider",
+    function($routeProvider, $locationProvider) {
+      $locationProvider.hashPrefix("");
+      $routeProvider
+        .when("/", {
+          template: "<welcome></welcome>"
+        })
+        .when("/login", {
+          template: "<login></login>"
+        })
+        .when("/dashboard", {
+          template: "<dashboard></dashboard>"
+        })
+        .when("/register", {
+          template: "<register></register>"
+        })
+        .when("/activate", {
+          template: "<activation></activation>"
+        })
+        .when("/privacy", {
+          template: "<privacy></privacy>"
+        })
+        .when("/products", {
+          template: "<products></products>"
+        })
+        .when("/websites", {
+          template: "<websites></websites>"
+        })
+        .otherwise({
+          redirectTo: "/"
+        });
+    }
+  ]);
+})();
--- public/app/index.js
+++ public/app/index.js
@@ -1,66 +1,12 @@
-const oren = angular.module("oren", ["ngRoute"])
+(function(){
+    const oren = angular.module("oren", ["ngRoute"]);
+    oren.config(['$compileProvider', function ($compileProvider) {
+        $compileProvider.debugInfoEnabled(false);
+        $compileProvider.commentDirectivesEnabled(false);
+        $compileProvider.cssClassDirectivesEnabled(false);
+      }]);
+}())
 
-.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.debugInfoEnabled(false);
-    $compileProvider.commentDirectivesEnabled(false);
-    $compileProvider.cssClassDirectivesEnabled(false);
-  }]);
-/**
- * Configuration
- * Goodwish Matyila
- * Application Router 
- */
-oren.config(["$routeProvider","$locationProvider",
-function($routeProvider, $locationProvider){
-  $locationProvider.hashPrefix("");
-  $routeProvider
-    .when("/", { 
-        template: "<welcome></welcome>" 
-    })
-    .when("/login",{ 
-        template: "<login></login>" 
-    })
-    .when("/dashboard", { 
-        template: "<dashboard></dashboard>"
-    })
-    .when("/register", { 
-        template: "<register></register>" 
-    })
-    .when("/activate", { 
-        template: "<activation></activation>" 
-    })
-    .when("/privacy", { 
-        template: "<privacy></privacy>" 
-    })
-    .when("/products", { 
-        template: "<products></products>" 
-    })
-    .when("/websites", { 
-        template: "<websites></websites>" 
-    })
-    .otherwise({ 
-        redirectTo: "/"
-    });
-}]);
-
-
-oren.controller("members-menu", ["$scope", function($scope){
-    let isUser = false; 
-    addEventListener("hashchange", function(){
-        firebase.auth().onAuthStateChanged((user)=>{
-            if(user){
-                $scope.isUser = true;
-            }else{
-                $scope.isUser = false;
-            }
-        })
-        $scope.$digest();
-    })
-}])
- 
-
-console.info("%cIf you looking here you should be working for us", "padding:2em;color:red;font-weight:900;font-size:20px");
-console.info("%csend CV goodwish@axxess.co.za", "padding:2em;color:green;font-weight:900;font-size:20px");
 
 
 
